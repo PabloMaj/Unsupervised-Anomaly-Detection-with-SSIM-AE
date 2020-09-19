25a0914a6e329033be220130cba6138ff6e58e2a
PabloMaj <majpaw1996@gmail.com>
2020-09-19 11:36:49 +0200

    Improvement code

---
+++ AE_evaluation.py
@@ -0,0 +1,195 @@
+import os
+import cv2
+from skimage.metrics import structural_similarity as ssim
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
+
+def calculate_TP_TN_FP_FN(ground_truth, predicted_mask):
+    TP = np.sum(np.multiply((ground_truth == predicted_mask), predicted_mask == 1))
+    TN = np.sum(np.multiply((ground_truth == predicted_mask), predicted_mask == 0))
+    FP = np.sum(np.multiply((ground_truth != predicted_mask), predicted_mask == 1))
+    FN = np.sum(np.multiply((ground_truth != predicted_mask), predicted_mask == 0))
+    return TP, TN, FP, FN
+
+
+def AP(TP, TN, FP, FN):
+    return (TP+TN)/(TP+TN+FP+FN)
+
+
+def DICE(TP, TN, FP, FN):
+    if (2*TP+FP+FN) == 0:
+        return 1
+    else:
+        return (2*TP)/(2*TP+FP+FN)
+
+
+def FPR(TP, TN, FP, FN):
+    return FP/(FP+TN)
+
+
+def TPR(TP, TN, FP, FN):
+    return TP/(TP+FN)
+
+
+def Youden_statistic(TP, TN, FP, FN):
+    if (TP+FN) == 0 or (TN+FP) == 0:
+        return None
+    else:
+        return TP/(TP+FN) + TN/(TN+FP) - 1
+
+
+def calculate_AUC(ROC_curve):
+    AUC = 0
+    FPR_values = [x[0] for x in ROC_curve]
+    TPR_values = [x[1] for x in ROC_curve]
+    for i in range(0, len(FPR_values)-1):
+        a = TPR_values[i]
+        b = TPR_values[i+1]
+        h = FPR_values[i+1]-FPR_values[i]
+        AUC += (a+b)*h/2
+    return AUC
+
+
+def plot_ROC_curve(metric_results):
+    FPR_values = [x[1] for x in metric_results["FPR"]]
+    TPR_values = [x[1] for x in metric_results["TPR"]]
+    plt.plot(FPR_values, TPR_values, "-o")
+
+
+def plot_YoundStat_thresh(metric_results):
+    YoundStat_values = [x[1] for x in metric_results["YoundenStat"]]
+    thresh_values = [x[0] for x in metric_results["YoundenStat"]]
+    plt.plot(thresh_values, YoundStat_values, "-o")
+
+
+def plot_DICE_thresh(metric_results):
+    DICE_values = [x[1] for x in metric_results["DICE"]]
+    thresh_values = [x[0] for x in metric_results["DICE"]]
+    plt.plot(thresh_values, DICE_values, "-o")
+
+
+def read_data(dataset):
+
+    img_oryg_samples = []
+    img_predict_samples = []
+    ground_truth_samples = []
+
+    for category in tqdm(os.listdir(f".data/{dataset}/test/")):
+        if category == "good":
+            continue
+        for img_name in os.listdir(f"data/{dataset}/test/{category}/"):
+            img_predict = cv2.imread(f"{dataset}/predicted/{category}/{img_name}", 0)
+
+            img_oryg = cv2.imread(f"data/{dataset}/test/{category}/{img_name}", 0)
+            img_oryg = cv2.resize(img_oryg, img_predict.shape)
+
+        if dataset in ["wool_1", "wool_2"]:
+            ground_truth = cv2.imread(f"data/{dataset}/ground_truth/{category}/{img_name}", 0)
+        else:
+            ground_truth = cv2.imread(f"data/{dataset}/ground_truth/{category}/{img_name[:-4]}_mask.png", 0)
+        ground_truth = cv2.resize(ground_truth, img_predict.shape)
+        ground_truth = (ground_truth > 0).astype(int)
+
+        img_oryg_samples.append(img_oryg)
+        img_predict_samples.append(img_predict)
+        ground_truth_samples.append(ground_truth)
+
+    return img_oryg_samples, img_predict_samples, ground_truth_samples
+
+
+def calculate_loss(img_oryg_samples, img_predict_samples, win_size=11):
+
+    loss_samples = []
+    n = len(img_oryg_samples)
+    for i in range(0, n):
+        img_oryg = img_oryg_samples[i]
+        img_predict = img_predict_samples[i]
+        _, S = ssim(img_oryg, img_predict, gradient=False, full=True, multichannel=False, win_size=win_size)
+        loss = 1/2 - S/2
+        loss_samples.append(loss)
+
+    return loss_samples
+
+
+def calculate_metrics(loss_samples, ground_truth_samples, thresh_max=1):
+
+    metric_results = dict()
+    metric_results["DICE"] = []
+    metric_results["YoundenStat"] = []
+    metric_results["TPR"] = []
+    metric_results["FPR"] = []
+
+    thresh_min = 0
+    thresh_values = np.linspace(thresh_min, np.sqrt(thresh_max), num=100)**2
+    thresh_values = [x for x in thresh_values]
+
+    for thresh in thresh_values:
+        Dice_values = []
+        YoundenStat_values = []
+        TPR_values = []
+        FPR_values = []
+        n = len(loss_samples)
+        for i in range(0, n):
+            predicted_mask = (loss_samples[i] > thresh).astype(int)
+            ground_truth = ground_truth_samples[i]
+            TP, TN, FP, FN = calculate_TP_TN_FP_FN(ground_truth=ground_truth, predicted_mask=predicted_mask)
+            Dice_values.append(DICE(TP, TN, FP, FN))
+            YoundenStat_values.append(Youden_statistic(TP, TN, FP, FN))
+            TPR_values.append(TPR(TP, TN, FP, FN))
+            FPR_values.append(FPR(TP, TN, FP, FN))
+        metric_results["DICE"].append((thresh, np.mean(Dice_values)))
+        metric_results["YoundenStat"].append((thresh, np.mean(YoundenStat_values)))
+        metric_results["TPR"].append((thresh, np.mean(TPR_values)))
+        metric_results["FPR"].append((thresh, np.mean(FPR_values)))
+    DICE_max_thresh, DICE_max = max(metric_results["DICE"], key=lambda x: x[1])
+    YoundenStat_max_thresh, YoundenStat_max = max(metric_results["YoundenStat"], key=lambda x: x[1])
+    ROC_curve = list(zip([x[1] for x in metric_results["FPR"]], [x[1] for x in metric_results["TPR"]]))
+    ROC_curve = sorted(ROC_curve, key=lambda x: x[1])
+    AUC = calculate_AUC(ROC_curve)
+
+    return metric_results, DICE_max, YoundenStat_max, AUC
+
+
+def create_predicted(dataset_name="carpet"):
+
+    ROI_resized_size = (128, 128)
+
+    for category in tqdm(os.listdir(f"data/{dataset_name}/test/")):
+
+        if category not in os.listdir(f"results/{dataset_name}/predicted/"):
+            os.mkdir(f"results/{dataset_name}/predicted/{category}/")
+
+        if category not in os.listdir(f"results/{dataset_name}/loss/"):
+            os.mkdir(f"results/{dataset_name}/loss/{category}/")
+
+    for img_name in tqdm(os.listdir(f"data/{dataset_name}/test/{category}/")):
+
+        img_in = cv2.imread(f"data/{dataset_name}/test/{category}/{img_name}", 0)
+        img_in = cv2.resize(img_in, ROI_resized_size)
+        img_in = img_in.astype("float32") / 255.0
+        X_test = []
+        X_test.append(img_in)
+        X_test = np.array(X_test)
+        X_test = np.expand_dims(X_test, axis=-1)
+        img_predict = autoencoder.predict(X_test)
+
+    cv2.imwrite(f"results/{dataset_name}/predicted/{category}/{img_name}",img_predict[0,:,:,:]*255)
+
+    mssim, grad, S = img_ssim = ssim(img_in[1:-1,1:-1],img_predict[0,1:-1,1:-1,0],gradient=True,full=True,multichannel=False)
+    
+    plt.clf()
+    plt.imshow(1-S,vmax=1.5,cmap="jet")
+    plt.colorbar()
+
+    plt.savefig(f"results/{dataset_name}/loss/{category}/{img_name}")
+
+
+if __name__ == "__main__":
+    datasets = ["grid", "wool_1", "wool_2", "carpet"]
+    for dataset in datasets:
+        img_oryg_samples, img_predict_samples, ground_truth_samples = read_data(dataset=dataset)
+        loss_samples = calculate_loss(img_oryg_samples=img_oryg_samples, img_predict_sample=img_predict_samples, win_size=11)
+        thresh_max = np.max(np.array(loss_samples))
+        metric_results, DICE_max, YoundenStat_max, AUC = calculate_metrics(loss_samples=loss_samples, ground_truth_samples=ground_truth_samples, thresh_max=thresh_max)
--- AE_training.py
+++ AE_training.py
@@ -1,238 +1,209 @@
 import cv2
 import os
-from tqdm import tqdm
 import numpy as np
 import random
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 import warnings
+import argparse
+import tensorflow as tf
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, Conv2DTranspose, LeakyReLU
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+
 warnings.simplefilter("ignore")
 
-from tensorflow.keras.callbacks import *
-from tensorflow.keras.optimizers import *
-from tensorflow.keras.models import *
-from tensorflow.keras.layers import *
-from tensorflow.keras import backend as K
-from tensorflow.keras import Sequential
-import tensorflow as tf
 
 def architecture_MVTEC(input_shape=(128, 128, 1), latent_dim=100):
 
-  #-----------------------------------
-  #encoder
+    parameters = dict()
+    N_layers = 9
+    parameters["filters"] = [32, 32, 32, 64, 64, 128, 64, 32, latent_dim]
+    parameters["kernel_size"] = [4, 4, 3, 4, 3, 4, 3, 3, 8]
+    parameters["strides"] = [2, 2, 1, 2, 1, 2, 1, 1, 1]
+    parameters["padding"] = ["same" for _ in range(N_layers-1)] + ["valid"]
 
-  #Input
-  inputs = Input(shape=input_shape)
-  x = inputs
+    # Input
+    inputs = Input(shape=input_shape)
+    x = inputs
 
-  #Conv1 (encoder)
-  x = Conv2D(filters = 32, kernel_size = (4, 4), strides = (2,2), padding = "same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
+    # Encoder
+    for i in range(0, N_layers):
+        x = Conv2D(
+          filters=parameters["filters"][i],
+          kernel_size=parameters["kernel_size"][i],
+          strides=parameters["strides"][i],
+          padding=parameters["padding"][i])(x)
+        x = LeakyReLU(alpha=0.2)(x)
 
-  #Conv2 (encoder)
-  x = Conv2D(filters = 32, kernel_size = (4, 4), strides = (2,2), padding = "same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
+    # Decoder
+    for i in reversed(range(0, N_layers)):
+        x = Conv2DTranspose(
+          filters=parameters["filters"][i],
+          kernel_size=parameters["kernel_size"][i],
+          strides=parameters["strides"][i],
+          padding=parameters["padding"][i])(x)
+        x = LeakyReLU(alpha=0.2)(x)
 
-  #Conv3 (encoder)
-  x = Conv2D(filters = 32, kernel_size = (3, 3), strides = (1,1), padding = "same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
+    # Output
+    x = Conv2DTranspose(
+      filters=input_shape[2],
+      kernel_size=(3, 3),
+      strides=(1, 1),
+      padding="same")(x)
 
-  #Conv4 (encoder)
-  x = Conv2D(filters = 64, kernel_size = (4, 4), strides = (2,2), padding = "same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
+    outputs = x
 
-  #Conv5 (encoder)
-  x = Conv2D(filters = 64, kernel_size = (3, 3), strides = (1,1), padding = "same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
+    # Autoencoder
+    autoencoder = Model(inputs, outputs, name="autoencoder")
 
-  #Conv6 (encoder)
-  x = Conv2D(filters = 128, kernel_size = (4, 4), strides = (2,2), padding = "same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
-
-  #Conv7 (encoder)
-  x = Conv2D(filters = 64, kernel_size = (3, 3), strides = (1,1), padding = "same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
-
-  #Conv8 (encoder)
-  x = Conv2D(filters = 32, kernel_size = (3, 3), strides = (1,1), padding = "same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
-
-  #Conv9 (encoder)
-  x = Conv2D(filters = latent_dim, kernel_size = (8, 8), strides = (1,1), padding = "valid")(x)
-  x = LeakyReLU(alpha=0.2)(x)  
-
-  #-----------------------------------
-  #decoder
-
-  #Conv9 (decoder)
-  x = Conv2DTranspose(filters = latent_dim, kernel_size = (8, 8), strides = (1,1), padding = "valid")(x)
-  x = LeakyReLU(alpha=0.2)(x)
-
-  #Conv8 (decoder)
-  x = Conv2DTranspose(filters = 32, kernel_size = (3, 3), strides = (1,1), padding = "same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
-
-  #Conv7 (decoder)
-  x = Conv2DTranspose(filters = 64, kernel_size = (3, 3), strides=(1,1), padding="same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
-
-  #Conv6 (decoder)
-  x = Conv2DTranspose(filters = 128, kernel_size = (4, 4), strides = (2,2), padding = "same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
-
-  #Conv5 (decoder)
-  x = Conv2DTranspose(filters = 64, kernel_size = (3, 3), strides = (1,1), padding = "same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
-
-  #Conv4 (decoder)
-  x = Conv2DTranspose(filters = 64, kernel_size = (4, 4), strides = (2,2), padding = "same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
-
-  #Conv3 (decoder)
-  x = Conv2DTranspose(filters = 32, kernel_size = (3, 3), strides = (1,1), padding = "same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
-
-  #Conv2 (decoder)
-  x = Conv2DTranspose(filters = 32, kernel_size = (4, 4), strides = (2,2), padding = "same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
-
-  #Conv1 (decoder)
-  x = Conv2DTranspose(filters = 32, kernel_size = (4, 4), strides = (2,2), padding = "same")(x)
-  x = LeakyReLU(alpha=0.2)(x)
-
-  x = Conv2DTranspose(filters = input_shape[2],  kernel_size = (3, 3), strides=(1,1), padding="same")(x)
-  outputs = x
-  
-  #-----------------------------------
-
-  #-----------------------------------
-  #autoencoder
-  autoencoder = Model(inputs, outputs, name="autoencoder")
-  #-----------------------------------
-
-  return autoencoder
-
-def read_data(dataset_name = "carpet"):
-
-  path_to_train_set = f"data/{dataset_name}/train/"
-  path_to_test_set =  f"data/{dataset_name}/test/"
-
-  train_data = dict()
-  test_data  = dict()
-
-  ROI_extracted_size = (256,256)
-  ROI_resized_size = (128,128)
-
-  for path_to_data , data_dict in [(path_to_train_set,train_data),(path_to_test_set,test_data)]:
-    for category in tqdm(os.listdir(path_to_data)):
-      for img_name in tqdm(os.listdir(path_to_data + str(category))):
-
-        img = cv2.imread(f"{path_to_data}{category}/{img_name}",0)
-        
-        for x_start in range(0,img.shape[0]-ROI_extracted_size[0]+1,ROI_extracted_size[0]):
-          for y_start in range(0,img.shape[1]-ROI_extracted_size[1]+1,ROI_extracted_size[1]):
-
-            x_end = x_start + ROI_extracted_size[0]
-            y_end = y_start + ROI_extracted_size[1]
-
-            img_ROI = img[x_start:x_end,y_start:y_end]
-            img_resized = cv2.resize(img_ROI, ROI_resized_size)
-            img_resized = img_resized.astype("float32") / 255.0
-
-            if category not in data_dict:
-              data_dict[category] = dict()
-            if img_name not in data_dict[category]:
-              data_dict[category][img_name] = []
-            data_dict[category][img_name].append(img_resized)
-  
-  return train_data, test_data
-
-def read_data_with_random_crop(dataset_name = "carpet",N_train = 10**4):
-
-  path_to_train_set = f"data/{dataset_name}/train/"
-  
-  ROI_extracted_size = (256,256)
-  ROI_resized_size = (128,128)
-
-  train_data = []
-  
-  for i in tqdm(range(0,N_train)):
-    
-    img_name = random.choice(list(os.listdir(path_to_train_set + "good")))
-    img = cv2.imread(f"{path_to_train_set}good/{img_name}",0)
-    x_start = random.randint(0, img.shape[0]-ROI_extracted_size[0])
-    y_start = random.randint(0, img.shape[1]-ROI_extracted_size[1])
-    x_end = x_start + ROI_extracted_size[0]
-    y_end = y_start + ROI_extracted_size[1]
-
-    img_ROI = img[x_start:x_end,y_start:y_end]
-    img_resized = cv2.resize(img_ROI, ROI_resized_size)
-    img_resized = img_resized.astype("float32") / 255.0
-
-    train_data.append(img_resized)
-
-  X_train = np.array(train_data)
-
-  return X_train
-
-def SSIMLoss(y_true, y_pred):
-  return 1 - tf.reduce_mean(tf.image.ssim(y_true, y_pred, 1.0))
-
-def train_model(input_shape = (128,128,1), dataset_name = "carpet" ,latent_dim = 100, training_loss = "ssim", load_model = True, random_crop = False, batch_size = 8):
-
-  #1) read data
-  if random_crop==True:
-    X_train = read_data_with_random_crop(dataset_name = dataset_name, N_train = 10000)
-    X_train = np.expand_dims(X_train, axis=-1)
-  else:
-    train_data, _ = read_data(dataset_name=dataset_name)
-    X_train = []
-    for img_name in train_data['good'].keys():
-      for img in train_data['good'][img_name]:
-        X_train.append(img)
-    X_train = np.array(X_train)
-    X_train = np.expand_dims(X_train, axis=-1)
-
-  Y_train = X_train
-  X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.2, shuffle=True)
-
-  #2) create autoencoder
-  autoencoder = architecture_MVTEC(input_shape=input_shape,latent_dim=latent_dim)
-  autoencoder.summary()
-
-  #3) choose training_loss function and optimizer
-  opt = Adam(learning_rate = 2*10e-5)
-
-  if training_loss == "mse":
-    autoencoder.compile(loss="mse", optimizer=opt)
-  elif training_loss == "ssim":
-    autoencoder.compile(loss=SSIMLoss, optimizer=opt)
+    return autoencoder
 
 
-  #4) path to save model, callbacks, read weights
-  path_to_save_model = f"model_weights/{dataset_name}/"
-  name = f"a_{latent_dim}_loss_{training_loss}_batch_{batch_size}.hdf5"
-  path_to_save_model += name
+def read_data(dataset_name="carpet"):
 
-  model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(save_weights_only=False,filepath=path_to_save_model,monitor='val_loss',save_best_only=True)
-  early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=100, verbose=0, mode='auto')
+    path_to_train_set = f"data/{dataset_name}/train/"
+    path_to_test_set = f"data/{dataset_name}/test/"
 
-  #5) training model  
-  if load_model:
-    if name in os.listdir(f"model_weights/{dataset_name}"):
-      autoencoder.load_weights(path_to_save_model)
-      autoencoder.fit(X_train, Y_train, epochs=300, batch_size=batch_size, shuffle=True, validation_data=(X_val, Y_val),callbacks=[model_checkpoint_callback,early_stopping_callback])
-  else:
-    autoencoder.fit(X_train, Y_train, epochs=300, batch_size=batch_size, shuffle=True, validation_data=(X_val, Y_val),callbacks=[model_checkpoint_callback,early_stopping_callback])
+    train_data = dict()
+    test_data = dict()
+
+    ROI_extracted_size = (256, 256)
+    ROI_resized_size = (128, 128)
+
+    for path_to_data, data_dict in [(path_to_train_set, train_data), (path_to_test_set, test_data)]:
+        for category in tqdm(os.listdir(path_to_data)):
+            for img_name in tqdm(os.listdir(path_to_data + str(category))):
+
+                img = cv2.imread(f"{path_to_data}{category}/{img_name}",0)
+                for x_start in range(0, img.shape[0]-ROI_extracted_size[0]+1, ROI_extracted_size[0]):
+                    for y_start in range(0, img.shape[1]-ROI_extracted_size[1]+1, ROI_extracted_size[1]):
+
+                        x_end = x_start + ROI_extracted_size[0]
+                        y_end = y_start + ROI_extracted_size[1]
+
+                        img_ROI = img[x_start:x_end, y_start:y_end]
+                        img_resized = cv2.resize(img_ROI, ROI_resized_size)
+                        img_resized = img_resized.astype("float32") / 255.0
+
+                        if category not in data_dict:
+                            data_dict[category] = dict()
+                        if img_name not in data_dict[category]:
+                            data_dict[category][img_name] = []
+                        data_dict[category][img_name].append(img_resized)
+
+    return train_data, test_data
+
+
+def read_data_with_random_crop(dataset_name="carpet", N_train=10**4):
+
+    path_to_train_set = f"data/{dataset_name}/train/"
+
+    ROI_extracted_size = (256, 256)
+    ROI_resized_size = (128, 128)
+
+    train_data = []
+
+    for i in tqdm(range(0, N_train)):
+
+        img_name = random.choice(list(os.listdir(path_to_train_set + "good")))
+        img = cv2.imread(f"{path_to_train_set}good/{img_name}", 0)
+        x_start = random.randint(0, img.shape[0]-ROI_extracted_size[0])
+        y_start = random.randint(0, img.shape[1]-ROI_extracted_size[1])
+        x_end = x_start + ROI_extracted_size[0]
+        y_end = y_start + ROI_extracted_size[1]
+
+        img_ROI = img[x_start:x_end, y_start:y_end]
+        img_resized = cv2.resize(img_ROI, ROI_resized_size)
+        img_resized = img_resized.astype("float32") / 255.0
+
+        train_data.append(img_resized)
+
+    X_train = np.array(train_data)
+
+    return X_train
+
+
+def DSSIM_loss(y_true, y_pred):
+    return 1/2 - tf.reduce_mean(tf.image.ssim(y_true, y_pred, 1.0))/2
+
+
+def train_model(input_shape=(128, 128, 1), dataset_name="carpet", latent_dim=100, training_loss="ssim", load_model=True, random_crop=False, batch_size=8):
+
+    # 1) read data
+    if random_crop:
+        X_train = read_data_with_random_crop(dataset_name=dataset_name, N_train=10000)
+        X_train = np.expand_dims(X_train, axis=-1)
+    else:
+        train_data, _ = read_data(dataset_name=dataset_name)
+        X_train = []
+        for img_name in train_data['good'].keys():
+            for img in train_data['good'][img_name]:
+                X_train.append(img)
+        X_train = np.array(X_train)
+        X_train = np.expand_dims(X_train, axis=-1)
+
+    Y_train = X_train
+    X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.2, shuffle=True)
+
+    # 2) create autoencoder
+    autoencoder = architecture_MVTEC(input_shape=input_shape, latent_dim=latent_dim)
+    autoencoder.summary()
+
+    # 3) choose training_loss function and optimizer
+    opt = Adam(learning_rate=2*10e-5)
+
+    if training_loss == "mse":
+        autoencoder.compile(loss="mse", optimizer=opt)
+    elif training_loss == "ssim":
+        autoencoder.compile(loss=DSSIM_loss, optimizer=opt)
+
+    # 4) set callbacks
+    path_to_save_model = f"model_weights/{dataset_name}/"
+    name = f"a_{latent_dim}_loss_{training_loss}_batch_{batch_size}.hdf5"
+    path_to_save_model += name
+
+    model_checkpoint_callback = ModelCheckpoint(
+      save_weights_only=False, filepath=path_to_save_model,
+      monitor='val_loss', save_best_only=True)
+    early_stopping_callback = EarlyStopping(
+      monitor='val_loss', min_delta=0, patience=100, verbose=0, mode='auto')
+
+    # 5) load weights and training model
+    if load_model:
+        if name in os.listdir(f"model_weights/{dataset_name}"):
+            autoencoder.load_weights(path_to_save_model)
+
+    autoencoder.fit(
+      x=X_train, y=Y_train,
+      epochs=300, batch_size=batch_size,
+      shuffle=True, validation_data=(X_val, Y_val),
+      callbacks=[model_checkpoint_callback, early_stopping_callback])
+
+
+def parse_args():
+    parser = argparse.ArgumentParser('AE_SSIM')
+    parser.add_argument("--dataset_name", type=str, default="woven_fabric_2")
+    parser.add_argument("--latent_dim", type=int, default=100)
+    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--training_loss", type=str, default="ssim")
+    parser.add_argument("--load_model", type=bool, default=0)
+    parser.add_argument("--random_crop", type=bool, default=1)
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
-  input_shape  = (128,128,1) 
-  dataset_name = "woven_fabric_2"
-  latent_dim = 100
-  batch_size = 8
-  training_loss = "ssim"
-  load_model = False
-  random_crop = True
 
-  print(f"\n dataset_name={dataset_name}\n training_loss={training_loss}\n latent_dim={latent_dim}\n batch_size={batch_size}\n")
-  train_model(input_shape = input_shape,dataset_name = dataset_name,latent_dim = latent_dim,training_loss = training_loss,load_model = False,random_crop=random_crop, batch_size=batch_size)
+    args = parse_args()
+
+    print(
+     f"\n dataset_name={args.dataset_name}\n training_loss={args.training_loss}\n"
+     "latent_dim={args.latent_dim}\n batch_size={args.batch_size}\n"
+     "load_model={args.load_model}\n random_crop={args.random_crop}")
+
+    train_model(
+     input_shape=(128, 128, 1), dataset_name=args.dataset_name,
+     latent_dim=args.latent_dim, training_loss=args.training_loss,
+     load_model=args.load_model, random_crop=args.random_crop,
+     batch_size=args.batch_size)

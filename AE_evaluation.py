a27bbac17ff4d01d9be662512edcb79c22c56293
PabloMaj <majpaw1996@gmail.com>
2020-09-20 17:27:29 +0200

    Code improvement

--- AE_evaluation.py
+++ AE_evaluation.py
@@ -5,7 +5,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
 from AE_training import architecture_MVTEC
-from scipy.ndimage import gaussian_filter
+
+plt.rc('font', size=12)
+plt.rc('xtick', labelsize=12)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=12)    # fontsize of the tick labels
+plt.rc('legend', fontsize=12)    # legend fontsize
+plt.rc('figure', titlesize=12)  # fontsize of the figure title
+
 
 def calculate_TP_TN_FP_FN(ground_truth, predicted_mask):
     TP = np.sum(np.multiply((ground_truth == predicted_mask), predicted_mask == 1))
@@ -53,22 +59,47 @@ def calculate_AUC(ROC_curve):
     return AUC
 
 
-def plot_ROC_curve(metric_results):
-    FPR_values = [x[1] for x in metric_results["FPR"]]
-    TPR_values = [x[1] for x in metric_results["TPR"]]
-    plt.plot(FPR_values, TPR_values, "-o")
+def plot_ROC_curve(datasets, results):
+    plt.clf()
+    for metric_results in results:
+        FPR_values = [x[1] for x in metric_results["FPR"]]
+        TPR_values = [x[1] for x in metric_results["TPR"]]
+        plt.plot(FPR_values, TPR_values, "-o")
+    plt.xlim(0, 1)
+    plt.xlabel("FPR", fontsize=12)
+    plt.ylabel("TPR")
+    plt.title("ROC Curve")
+    plt.legend(datasets)
+    plt.grid()
+    plt.savefig("ROC_curve.png")
 
 
-def plot_YoundStat_thresh(metric_results):
-    YoundStat_values = [x[1] for x in metric_results["YoundenStat"]]
-    thresh_values = [x[0] for x in metric_results["YoundenStat"]]
-    plt.plot(thresh_values, YoundStat_values, "-o")
+def plot_YoundStat_thresh(datasets, results):
+    plt.clf()
+    for metric_results in results:
+        YoundStat_values = [x[1] for x in metric_results["YoundenStat"]]
+        thresh_values = [x[0] for x in metric_results["YoundenStat"]]
+        plt.plot(thresh_values, YoundStat_values, "-o")
+    plt.xlabel("Threshold")
+    plt.ylabel("Younden's Statistic")
+    plt.title("Younden's Statistic (Threshold)")
+    plt.legend(datasets)
+    plt.grid()
+    plt.savefig("YoundStat_thresh.png")
 
 
-def plot_DICE_thresh(metric_results):
-    DICE_values = [x[1] for x in metric_results["DICE"]]
-    thresh_values = [x[0] for x in metric_results["DICE"]]
-    plt.plot(thresh_values, DICE_values, "-o")
+def plot_DICE_thresh(datasets, results):
+    plt.clf()
+    for metric_results in results:
+        DICE_values = [x[1] for x in metric_results["DICE"]]
+        thresh_values = [x[0] for x in metric_results["DICE"]]
+        plt.plot(thresh_values, DICE_values, "-o")
+    plt.xlabel("Threshold")
+    plt.ylabel("DICE")
+    plt.title("DICE (Threshold)")
+    plt.legend(datasets)
+    plt.grid()
+    plt.savefig("DICE_thresh.png")
 
 
 def read_data(dataset):
@@ -84,13 +115,11 @@ def read_data(dataset):
             img_predict = cv2.imread(f"results/{dataset}/predicted/{category}/{img_name}", 0)
 
             img_oryg = cv2.imread(f"data/{dataset}/test/{category}/{img_name}", 0)
-            img_oryg = cv2.resize(img_oryg, img_predict.shape)
 
-        if dataset in ["wool_1", "wool_2"]:
+        if dataset in ["texture_1", "texture_2"]:
             ground_truth = cv2.imread(f"data/{dataset}/ground_truth/{category}/{img_name}", 0)
         else:
             ground_truth = cv2.imread(f"data/{dataset}/ground_truth/{category}/{img_name[:-4]}_mask.png", 0)
-        ground_truth = cv2.resize(ground_truth, img_predict.shape)
         ground_truth = (ground_truth > 0).astype(int)
 
         img_oryg_samples.append(img_oryg)
@@ -105,11 +134,11 @@ def calculate_loss(img_oryg_samples, img_predict_samples, win_size=11):
     loss_samples = []
     n = len(img_oryg_samples)
     for i in range(0, n):
-        img_oryg = img_oryg_samples[i]
-        img_predict = img_predict_samples[i]
-        _, S = ssim(img_oryg, img_predict, gradient=False, full=True, multichannel=False, win_size=win_size)
+        img_oryg = img_oryg_samples[i].astype("float32") / 255.0
+        img_predict = img_predict_samples[i].astype("float32") / 255.0
+        _, S = ssim(
+            X=img_oryg, Y=img_predict, data_range=1, gradient=False, full=True, multichannel=False, win_size=win_size)
         loss = 1/2 - S/2
-        loss = gaussian_filter(loss, sigma=4)
         loss_samples.append(loss)
 
     return loss_samples
@@ -124,7 +153,7 @@ def calculate_metrics(loss_samples, ground_truth_samples, thresh_max=1):
     metric_results["FPR"] = []
 
     thresh_min = 0
-    thresh_values = np.linspace(thresh_min, np.sqrt(thresh_max), num=100)**2
+    thresh_values = np.linspace(thresh_min, thresh_max, num=100)**2
     thresh_values = [x for x in thresh_values]
 
     for thresh in thresh_values:
@@ -148,7 +177,7 @@ def calculate_metrics(loss_samples, ground_truth_samples, thresh_max=1):
     DICE_max_thresh, DICE_max = max(metric_results["DICE"], key=lambda x: x[1])
     YoundenStat_max_thresh, YoundenStat_max = max(metric_results["YoundenStat"], key=lambda x: x[1])
     ROC_curve = list(zip([x[1] for x in metric_results["FPR"]], [x[1] for x in metric_results["TPR"]]))
-    ROC_curve = sorted(ROC_curve, key=lambda x: x[1])
+    ROC_curve = sorted(ROC_curve, key=lambda x: x[0])
     AUC = calculate_AUC(ROC_curve)
 
     return metric_results, DICE_max, YoundenStat_max, AUC
@@ -162,10 +191,16 @@ def create_predicted(dataset_name="carpet", latent_dim=100, training_loss="ssim"
     path_to_load_model += name
     autoencoder.load_weights(path_to_load_model)
 
-    img_in_size = (1024, 1024)
-    img_resized_size = (256, 256)
-    crop_size = (128, 128)
-    step = 16
+    if dataset_name in ["texture_1", "texture_2"]:
+        img_in_size = (512, 512)
+        img_resized_size = (256, 256)
+        crop_size = (128, 128)
+        step = 16
+    elif dataset_name in ["carpet", "grid"]:
+        img_in_size = (1024, 1024)
+        img_resized_size = (512, 512)
+        crop_size = (128, 128)
+        step = 32
 
     for category in tqdm(os.listdir(f"data/{dataset_name}/test/")):
 
@@ -195,15 +230,15 @@ def create_predicted(dataset_name="carpet", latent_dim=100, training_loss="ssim"
                     y_start = y
                     y_end = y + crop_size[1]
                     crop = img_resized[x_start:x_end, y_start:y_end]
-                    
+
                     X_test = []
                     X_test.append(crop)
                     X_test = np.array(X_test)
                     X_test = np.expand_dims(X_test, axis=-1)
                     img_predict = autoencoder.predict(X_test)
 
-                    img_out[x_start:x_end, y_start:y_end] += img_predict[0,:,:,0]
-                    overlap[x_start:x_end, y_start:y_end] += np.ones(shape=crop_size)
+                    img_out[(x_start+1):(x_end-1), (y_start+1):(y_end-1)] += img_predict[0, 1:-1, 1:-1, 0]
+                    overlap[(x_start+1):(x_end-1), (y_start+1):(y_end-1)] += np.ones(shape=crop_size)[1:-1, 1:-1]
 
             overlap = np.where(overlap == 0, 1, overlap)
             img_out = img_out/overlap
@@ -221,12 +256,29 @@ def create_predicted(dataset_name="carpet", latent_dim=100, training_loss="ssim"
 
 
 if __name__ == "__main__":
-    datasets = ["grid"]
+
+    datasets = ["grid", "texture_1", "texture_2", "carpet"]
+    results = []
+
     for dataset in datasets:
-        #create_predicted(dataset_name=dataset)
+
+        # 1) Create predicted images
+        create_predicted(dataset_name=dataset)
+
+        # 2) Read original, predcited images and ground truth
         img_oryg_samples, img_predict_samples, ground_truth_samples = read_data(dataset=dataset)
-        loss_samples = calculate_loss(img_oryg_samples=img_oryg_samples, img_predict_samples=img_predict_samples, win_size=11)
+
+        # 3) Calculate loss
+        loss_samples = calculate_loss(
+            img_oryg_samples=img_oryg_samples, img_predict_samples=img_predict_samples, win_size=11)
         thresh_max = np.max(np.array(loss_samples))
-        metric_results, DICE_max, YoundenStat_max, AUC = calculate_metrics(loss_samples=loss_samples, ground_truth_samples=ground_truth_samples, thresh_max=thresh_max)
-        print(f"dataset:{dataset}\nAUC:{AUC}\n"
-        f"YoundenStat_max:{YoundenStat_max}\nDICE_max:{DICE_max}\n")
+
+        # 4) Calculate metrics
+        metric_results, DICE_max, YoundenStat_max, AUC = calculate_metrics(
+            loss_samples=loss_samples, ground_truth_samples=ground_truth_samples, thresh_max=thresh_max)
+        print(f"dataset:{dataset}\nAUC:{AUC}\n"f"YoundenStat_max:{YoundenStat_max}\nDICE_max:{DICE_max}\n")
+        results.append(metric_results)
+
+    plot_ROC_curve(datasets=datasets, results=results)
+    plot_YoundStat_thresh(datasets=datasets, results=results)
+    plot_DICE_thresh(datasets=datasets, results=results)
